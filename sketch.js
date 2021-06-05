18ce2bdc5f2709a4252289a188b490a26f363395
MahiVarshni <84715191+MahiVarshni@users.noreply.github.com>
2021-06-05 18:59:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+var bg,backgroundImg;
+var cat,mouse;
+var catImg1,catImg2,catImg4,mouseImg4,mouseImg1,mouseImg2;
+
+
+function preload() {
+    //load the images here
+    backgroundImg = loadImage("images/garden.png");
+    catImg1=loadImage("images/cat1.png");
+    catImg2=loadImage("images/cat2.png ","images/cat3.png");
+    catImg4=loadImage("images/cat4.png");
+    mouseImg1=loadImage("images/mouse1.png");
+    mouseImg2=loadImage("images/mouse2.png ","images/mouse3.png");
+    mouseImg4=loadImage("images/mouse4.png");
+}
+
+function setup(){
+    createCanvas(1000,800);
+
+    bg =createSprite(500,200);
+    bg.addAnimation("bb",backgroundImg);
+    
+
+    cat =createSprite(750,450);
+    cat.addAnimation("bbc",catImg1);
+    cat.scale=0.1;
+
+   mouse=createSprite(200,450);
+   mouse.addAnimation("aaa",mouseImg1);
+   mouse.scale=0.1;
+
+  
+
+
+}
+
+function draw() {
+
+    background(0);
+   if (cat.x-mouse.x<(cat.width-mouse.width)){
+cat.velocityX=0;
+cat.x=300;
+cat. addAnimation("win",catImg4);
+cat. changeAnimation("win");
+mouse. addAnimation("ddd",mouseImg4);
+mouse. changeAnimation("ddd");
+   }
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+  if (keyCode === LEFT_ARROW){
+  cat.addAnimation("catRunning",catImg2);
+  cat.changeAnimation("catRunning");
+  cat.velocityX=-2;
+  mouse.addAnimation("mouseTeasing",mouseImg2);
+  mouse.changeAnimation("mouseTeasing");
+  }
+
+}
